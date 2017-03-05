dac210e37a006a102a97908d3478e28d760757d2
Lauro Cruz <laurocruzsouza@gmail.com>
2017-03-05 17:05:43 -0300

    fixed calc_primo

--- ex1/calc_primo.c
+++ ex1/calc_primo.c
@@ -3,7 +3,11 @@
 int primo(int n) {
     int i;
 
-    if (n % 2 == 0) return 0;
+    if (n % 2 == 0) {
+        if (n != 2) return 0;
+        else return 1;
+    }
+
 
     for(i = 3; i < n; i += 2)
         if (n % i == 0)
