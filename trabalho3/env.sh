d57ff687cb253c6114831a40c7d1c7fd31b7a1ac
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-06-23 02:20:32 +0000

    Script remove a pasta

--- trabalho3/env.sh
+++ trabalho3/env.sh
@@ -3,6 +3,7 @@
 # This script creates the envirement to run the project inside the virtual
 # machine offered on the discipline or on the computers prepared for it
 
+rm -rf mips-tlm
 wget http://www.ic.unicamp.br/~lucas/teaching/mc723/2017-1/mips-tlm-per.tar.gz
 tar xzf mips-tlm-per.tar.gz
 rm mips-tlm-per.tar.gz
